0bfe47d5e0dcf99f18552e5e335cd14b000ce359
1YGZX6RVhv <amtgpj387@gmail.com>
2022-09-01 21:26:19 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     console.log("fetching nfts");
 
     // const api_key = ""
-    const api_key = "3sSrhmP0ew0SwPKeIYE_wZWYnw2vCkWG"
+    const api_key = "EgNkZhLor-bZTnmihfct5XwxEFfJMA0Z"
 
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
@@ -43,7 +43,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = "3sSrhmP0ew0SwPKeIYE_wZWYnw2vCkWG"
+      const api_key = "EgNkZhLor-bZTnmihfct5XwxEFfJMA0Z"
 
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
